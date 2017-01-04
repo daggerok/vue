d99d7ad4e9864a3cb6f13ea84c42da04a69581af
Maksim Kostromin <daggerok@gmail.com>
2017-01-05 00:36:37 +0200

    add event hendling

--- app.js
+++ app.js
@@ -2,11 +2,34 @@
 
 (function () {
 
-  return new Vue({
-    el: '#app',
+  new Vue({
+    el: '#v-on',
+    methods: {
+      handler: function(event) {
+        // event.preventDefault();
+        console.log('hi', event);
+      },
+    },
+  });
+
+  new Vue({
+    el: '#v-show-v-if',
+    data: {
+      message: '',
+    },
+    methods: {
+      handler: function(event) {
+        event.preventDefault();
+        console.log('clicked', JSON.stringify(this.$data));
+        this.$data.message = '';
+      },
+    },
+  });
+
+  new Vue({
+    el: '#two-way-binding',
     data: {
       message: 'hi!',
-      longMessage: '',
     },
   });
 
